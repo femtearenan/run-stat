514f5dfd930d9ed0f0a297e66cb7c0a2c42de002
Anders Björkland <anders@femtearenan.se>
2020-05-02 21:37:47 +0200

    added more style

--- src/components/BarChart.js
+++ src/components/BarChart.js
@@ -10,17 +10,20 @@ class BarChart extends React.Component {
 
     drawChart() {
         if (this.props.isOK) {
-            const height = 400;
+            const height = 300;
             const width = 200;
             const padding = 30;
 
             const svg = d3.select(`#chart-${this.props.id}`)
                 .append("svg")
+                .attr("id", () => "barchart-" + this.props.id)
+                .attr("class", () => "barchart")
                 .attr("width", width)
                 .attr("height", height);
 
             const runData = this.props.runData;
             const typeData = this.props.typeData;
+            console.log(typeData);
 
             const normalRun = runData.filter(run => run.type === "/api/run_types/1");
             const intervalRun = runData.filter(run => run.type !== "/api/run_types/1");
@@ -29,32 +32,57 @@ class BarChart extends React.Component {
                     .domain([0, d3.max(typeData, (d) => d.runs.length)])
                     .range([padding, height - padding]);
 
+            let rectId = 0;
+
+            //height - yScale(d.runs.length)
             svg.selectAll("rect")
                 .data(typeData)
                 .enter()
                 .append("rect")
-                .attr("x", (d, i) => i * 55)
-                .attr("y", (d, i) => height - yScale(d.runs.length) )
+                .attr("id", () => "rect-" + rectId++ )
+                .attr("x", (d, i) => i * 75)
+                .attr("y", (d, i) => 30)
                 .attr("width", 50)
                 .attr("height", (d, i) => yScale(d.runs.length));
+
+            svg.selectAll("text")
+                .data(typeData)
+                .enter()
+                .append("text")
+                .text((d) =>  (d.name))
+                .attr("class", "bar-text")
+                .attr("x", (d, i) => i * 75)
+                .attr("y", (d) => 0);
+
+            svg.selectAll("text")
+                .data(typeData)
+                .enter()
+                .append("text")
+                .text((d) =>  (d.runs.length))
+                .attr("class", "bar-info")
+                .attr("x", (d, i) => i * 75)
+                .attr("y", (d) => yScale(d.runs.length)/2);
         }
 
     }
 
+    
+
     render() {
         if (this.props.isOK) {
             return (
-                <div id={"chart-" + this.props.id} className="barChart" >
-                    <h2>This is a bar chart</h2>
-                    <p>Also, everything's OK!</p>
+                <div id={"chart-" + this.props.id} className="barchart-container" >
+                    <h2>Type of runs on record</h2>
                     {this.drawChart()}
+                    <p>Normal runs are classic distance runs. Interval runs are any of hill runs, fartlek or intervals. <br/>(Source: <a href="https://femtearenan.se">femtearenan.se</a>)</p>
+
                 </div>
             )
 
         } else {
             return (
-                <div id={"chart-" + this.props.id} className="barChart">
-                    <h2>This is a bar chart</h2>
+                <div id={"chart-" + this.props.id} className="barchart-container">
+                    <h2>Loading chart data</h2>
                 </div>
             );
         }
